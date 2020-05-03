d8b209f17586aa065452ff6e730cf51f4e73945e
Ian <iansedano@gmail.com>
2020-05-03 16:15:13 +0200

    Conceptualizing scope of project and planning.

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import os
 import tweepy
 import secret
 import json
-
+import sqlalchemy
 import sys
+from datetime import date, timedelta
+from pprint import pprint
 from io import TextIOWrapper
+
+
 sys.stdout = TextIOWrapper(
     sys.stdout.buffer, encoding='utf-8', errors='replace')
 
@@ -18,29 +22,82 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
 api = tweepy.API(auth)
 
-'''
-handle = 'elonmusk'
-user = api.get_user(handle)
-num_friends = user.friends_count
+search_term = "python"
 
-print(num_friends)
+#max tweets can be 100
+#no tweets older than 7 days will be found
+#max 450 requests 15 mins
+#max 300 requests per 3 hour window or 250 requests per month?
 
-#home timeline
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    # Process a single status
-    print(status.text)
+# lets do 28 searches per day for last 7 days 
+# = 196 requests
+# have to go from 7 days to today
+# 
 
-'''
+today = date.today()
+delta = timedelta(days=-1)
 
-tweets = tweepy.Cursor(api.search,
-	q="python",
-	lang="en",
-	since=2018-11-16).items(100)
+def get_tweets(search_term, json_path, type, date):
+	tweets = tweepy.Cursor(api.search,
+		q=search_term,
+		lang="en",
+		result_type=type, # maybe alternate between recent and popular
+		# maybe include a max_id and since_id here based on the tweets received, so as to get more tweets.
+		# maybe include certain geocodes (most popular tweeting cities?) latitude,longitude,radius (radius in mi), i.e "37.781157 -122.398720 1mi"
+		include_entities="false",
+		since=date).items(100)
 
-mylist =[]
-for t in tweets:
-	mylist.append(t._json)
+	mylist =[]
+	for t in tweets:
+		mylist.append(t._json)
+
+	with open(json_path,'w') as fp:
+		json.dump(mylist, fp)
+
+# testing
+
+get_tweets("coronavirus", "jsons\\test.json", "recent", str(today))
 
 
-with open('result.json','w') as fp:
-	json.dump(mylist, fp)
+
+def read_json(path):
+	with open(path) as json_file:
+		data = json.load(json_file)
+		for tweet in data:
+			print(tweet['text'])
+
+read_json("jsons\\test.json")
+
+def json_to_db(json_path):
+	
+
+	engine = sqlalchemy.create_engine(
+    	'mysql+pymysql://root:cO2rm2JY9mHNPqkr0GSs@localhost/twitbotto')
+
+	connection = engine.connect()
+	metadata = sqlalchemy.MetaData()
+	tweets = sqlalchemy.Table(
+	    'tweets', metadata, autoload=True, autoload_with=engine)
+	searches = sqlalchemy.Table(
+	    'searches', metadata, autoload=True, autoload_with=engine)
+	hashtags = sqlalchemy.Table(
+	    'hashtags', metadata, autoload=True, autoload_with=engine)
+	users = sqlalchemy.Table(
+	    'users', metadata, autoload=True, autoload_with=engine)
+
+
+
+	query = sqlalchemy.insert(newTable).values(
+	    id=1,
+	    name='Software Ninjaneer',
+	    salary=60000.00,
+	    active=True)
+
+	result_proxy = connection.execute(query)
+
+	query = sqlalchemy.insert(newTable)
+
+	new_records = [{'id': '2', 'name': 'record1', 'salary': 80000, 'active': False},
+	               {'id': '3', 'name': 'record2', 'salary': 70000, 'active': True}]
+
+	result_proxy = connection.execute(query, new_records)
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Twitter Wordcloud Generator
+
+## Aims
+
+* Mine twitter for tweets under a search term
+* Create wordcloud for the text in these tweets
+
+
